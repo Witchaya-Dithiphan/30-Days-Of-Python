f164d7f375b363bbbe1138eef0058663c891f9f3
Witchaya-Dithiphan <poom.witchaya@gmail.com>
2026-08-16 18:39:00 +0700

    complete exercise2 level1.

---
+++ 02_Day_Variables_builtin_functions/d2_level1.py
@@ -0,0 +1,12 @@
+#Day 2: 30 Days of python programming
+first_name = 'Witchaya'
+last_name = "Dithiphan"
+full_name = "Witchaya Dithiphan"
+country = "Thailand"
+city = "Pattaya"
+age = 20
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False 
+Nickname , university = "Poom" , "KMITL"
