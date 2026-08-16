a37bf6f8fadc61f45f17fa3d2f8ac63390a09e71
Witchaya-Dithiphan <poom.witchaya@gmail.com>
2026-08-16 17:05:47 +0700

    complete: exercise1 level3.

---
+++ 01_Day_Introduction/level3.py
@@ -0,0 +1,25 @@
+a = 15
+b = 3.14
+c = 4 -5j
+d = "Super_man"
+e = True
+f = [1, 2, 3, 4, 5]
+g = (1, 2, 3, 4, 5)
+h = {1,2,3,4,5}
+i = {"name": "Witchaya", "age": 20, "country": "Thailand"}
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
+point1 = (2, 3)
+point2 = (4,5)
+
+euclidean_distance = ((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)**0.5
+print("Euclidean distance between point1 and point2 is:", euclidean_distance)
