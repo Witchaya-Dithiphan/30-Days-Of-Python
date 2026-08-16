41ef4dd83f89e400da20837d1af0b8154eddb7fa
Witchaya-Dithiphan <poom.witchaya@gmail.com>
2026-08-16 16:58:38 +0700

    complete: exercise1 level2.

--- 01_Day_Introduction/level2.py
+++ 01_Day_Introduction/level2.py
@@ -3,6 +3,23 @@ surname = "Dithiphan"
 country = "Thailand"
 string = "I am enjoying 30 days of python"
 
+a = 5
+b = 17
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(b%a)
+print(b/a)
+print(a**b)
+print(b//a)
+
+
+print(name)
+print(surname)
+print(country)
+print(string)
+
 print(type(10))
 print(type(9.8))
 print(type(3.14))
