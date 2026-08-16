0c24623f150a2af9ca794d975f4cdce0b506ebd7
Witchaya-Dithiphan <poom.witchaya@gmail.com>
2026-08-16 16:54:10 +0700

    complete: exercise 1 level 1.

---
+++ 01_Day_Introduction/level1.py
@@ -0,0 +1,13 @@
+name = "Witchaya"
+surname = "Dithiphan"
+country = "Thailand"
+string = "I am enjoying 30 days of python"
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(surname))
+print(type(country))
